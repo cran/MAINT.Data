aaac6ee55d2f417b879eaa17bdc20ea858523b9d
Pedro Duarte Silva <psilva@porto.ucp.pt>
2011-08-03 00:00:00 +0000

    version 0.1

---
+++ R/ClasGenMetDef.R
@@ -0,0 +1,36 @@
+setClass("IData",representation(MidP="data.frame",LogR="data.frame",ObsNames="character",VarNames="character",NObs="numeric",NIVar="numeric"))
+setClass("IdtE",representation(ModelNames="character",ModelType="character",ModelConfig="numeric",NIVar="numeric",SelCrit="character",
+				logLiks="numeric",AICs="numeric",BICs="numeric",BestModel="numeric"),contains="VIRTUAL")
+setClass("IdtSngDE",contains=c("IdtE","VIRTUAL"))
+setClass("IdtMxE",representation(grouping="factor"),contains=c("IdtE","VIRTUAL"))
+setClass("IdtSngNDE",representation(mleNmuE="numeric",mleNmuEse="numeric",Configurations="list"),contains="IdtSngDE")
+setClass("IdtMxNDE",representation(Hmcdt="logical",mleNmuE="matrix",mleNmuEse="matrix",Configurations="list"),contains="IdtMxE")
+setClassUnion("IdtNDE",c("IdtSngNDE","IdtMxNDE"))
+setClass("LRTest",representation(QuiSq="numeric",df="numeric",pvalue="numeric",H0logLik="numeric",H1logLik="numeric"))
+setClass("ConfTests",representation(TestRes="list",RestModels="character",FullModels="character"))
+setClass("IdtMANOVA",representation(NIVar="numeric",grouping="factor",H0res="IdtSngDE",H1res="IdtMxE"),contains="LRTest")
+setClass("IdtClMANOVA",contains="IdtMANOVA")
+setClass("IdtHetNMANOVA",contains="IdtMANOVA")
+setClass("Idtlda",representation(prior="numeric",means="matrix",scaling="matrix",N="numeric"))
+setClass("Idtqda",representation(prior="numeric",means="matrix",scaling="array",ldet="numeric",lev="character"))
+
+setGeneric("summary")
+setGeneric("nrow")
+setGeneric("ncol")
+setGeneric("head",package="utils")
+setGeneric("tail",package="utils")
+setGeneric("coef",package="stats")
+setGeneric("stdEr",package="miscTools")
+setGeneric("lda",package="MASS")
+setGeneric("qda",package="MASS")
+setGeneric("predict",package="stats")
+
+setGeneric("mle",function(Idt,Model="Normal",Config=1:5,SelCrit=c("AIC","BIC"))  standardGeneric("mle"))
+setGeneric("MANOVA",function(Idt, grouping, Model="Normal", Config=1:5, SelCrit=c("AIC","BIC"), Mxt=c("Hom","Het"), tol=1.0e-4)  standardGeneric("MANOVA"))
+setGeneric("BestModel",function(Idt,SelCrit=c("IdtCrt","AIC","BIC"))  standardGeneric("BestModel"))
+setGeneric("testMod",function(Idt,RestMod=1:length(Idt@ModelNames),FullMod="Next")  standardGeneric("testMod"))
+setGeneric("H1res",  function(object) standardGeneric("H1res"))
+setGeneric("H0res",  function(object) standardGeneric("H0res"))
+
+
+
---
+++ R/tmp.R
@@ -0,0 +1,51 @@
+CovtoParC2 <- function(Sigma,q,Sqrt)	
+{					 
+# 	Gets the vectorized form of covariance matrix according to Configuration 2
+
+	intcomb <- q*(q+1)/2	   	    # Number of possible combinations between pairs of interval variables
+	par <- array(dim=2*intcomb+q)
+	if (Sqrt==TRUE) mat <- t(chol(Sigma))
+	else mat <- Sigma
+	cnt <- 1	
+	for (i in 1:q) {
+		for (j in 1:i)  {
+			par[cnt] <- mat[i,j] 
+			par[intcomb+cnt] <- mat[q+i,q+j]
+			cnt <- cnt + 1
+		}
+		par[2*intcomb+i] <- mat[q+i,i]
+	}
+	par
+} 
+
+PartoCovC2 <- function(par,q)
+
+#  Gets the covariance matrix from its vectorized form according to Configuration 2
+
+#  Arguments
+
+#  par    - The free Configuration 2 covariances in vector form
+#  q      - Number of integer variables
+  
+{
+	Sigma <- matrix(0.,2*q,2*q)
+	intcomb <- q*(q+1)/2		# Number of possible combinations between pairs of interval variables
+	cnt <- 1	
+	for (i in 1:q) {
+		for (j in 1:i)  {
+			Sigma[i,j] <- Sigma[j,i] <- par[cnt]  
+			Sigma[q+i,q+j] <- Sigma[q+j,q+i] <- par[intcomb+cnt]  
+			cnt <- cnt + 1
+		}
+		Sigma[q+i,i] <- Sigma[i,q+i] <- par[2*intcomb+i]  
+	}
+	Sigma
+}
+
+logLik <- function(Data,par,q)
+{
+	Sigma <- PartoCovC2(par,q)
+	spar <- CovtoParC2(Sigma,q,Sqrt=TRUE)
+	-GC2mLogLik(spar,Data)
+}	
+
